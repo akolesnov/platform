5e8945e21bfe9f4c6b4a7c801a45480a67d8d00f
akolesnov <a.kolesnov@yandex.ru>
2025-07-19 00:24:11 +0300

    Added basic middleware components

---
+++ pipeline/component.go
@@ -0,0 +1,22 @@
+package pipeline
+
+import "net/http"
+
+type ComponentContext struct {
+	*http.Request
+	http.ResponseWriter
+	error
+}
+
+func (mvc *ComponentContext) Error(err error) {
+	mvc.error = err
+}
+
+func (mvc *ComponentContext) GetError() error {
+	return mvc.error
+}
+
+type MiddlewareComponent interface {
+	Init()
+	ProcessRequest(context *ComponentContext, next func(*ComponentContext))
+}
